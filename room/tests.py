8e78bdb5c532735035859bce614ac21c2ccb2847
Paweł Młynarski <pawel.wybrzeze@gmail.com>
2019-11-24 18:39:47 +0100

    Skonczono testy w aplikacji 'room', zmiany w views.py

--- room/tests.py
+++ room/tests.py
@@ -1,3 +1,94 @@
-from django.test import TestCase
+from datetime import datetime, timedelta
 
-# Create your tests here.
+from django.urls import reverse, path, include
+from rest_framework import status
+from rest_framework.test import APITestCase, URLPatternsTestCase
+
+from reservation.models import Reservation
+from room import views
+from user.models import User
+from .models import Room
+import json
+
+
+class RoomTests(APITestCase, URLPatternsTestCase):
+    urlpatterns = [
+        path('rooms/show_available/rooms/', include('room.urls')),
+    ]
+
+    def test_showing_available_rooms(self):
+        url = reverse('show_available_rooms')
+        data = {'date': datetime.strftime(datetime.now() + timedelta(days=1), '%Y-%m-%d'),
+                'number_of_seats': '10',
+                'number_of_computers': '10',
+                'additional_equipment': 'BRAK',
+                'from_hour': '8:00',
+                'to_hour': '18:30'
+                }
+
+        test_room = Room.objects.create(number='126', wing='B2', number_of_seats=15, number_of_computers=15)
+        Room.objects.create(number='127', wing='B2', number_of_seats=15, number_of_computers=0)
+        test_user = User.objects.create(email='test@test.test', first_name='Andrzej', last_name='Testowy',
+                                        password='1234567890')
+        Reservation.objects.create(date=datetime.now() + timedelta(days=1),
+                                   hour=datetime.strptime('8:00', '%H:%M').time(), room=test_room, user=test_user)
+        response = self.client.post(url, data=data, format='json')
+        print('----------WYSWIETLANIE DOSTĘPNYCH GODZIN GDY DANE SIĘ ZGADZAJĄ----------')
+        print(json.loads(response.content))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content),
+                         {'126 B2': ['09:45:00', '11:30:00', '13:15:00', '15:00:00', '16:45:00', '18:30:00']})
+        print('----------WYSWIETLANIE DOSTĘPNYCH GODZIN GDY DANE SIĘ ZGADZAJĄ, DLA DWÓCH SAL----------')
+        data['number_of_seats'] = '5'
+        data['number_of_computers'] = '0'
+        response = self.client.post(url, data=data, format='json')
+        print(json.loads(response.content))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content),
+                         {'126 B2': ['09:45:00',
+                                     '11:30:00',
+                                     '13:15:00',
+                                     '15:00:00',
+                                     '16:45:00',
+                                     '18:30:00'],
+                          '127 B2': ['08:00:00',
+                                     '09:45:00',
+                                     '11:30:00',
+                                     '13:15:00',
+                                     '15:00:00',
+                                     '16:45:00',
+                                     '18:30:00']}
+                         )
+        data['from_hour'] = '8;00'
+        data['to_hour'] = '18.30'
+        response = self.client.post(url, data=data, format='json')
+        print('----------SPRAWDZANIE CZY FORMAT GODZINY JEST ODPOWIEDNI----------')
+        print(json.loads(response.content))
+        self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+        self.assertEqual(json.loads(response.content),
+                         {'error': 'Zły format godziny. Prawidłowy format godziny to hh:mm'})
+        data['from_hour'] = '8:00'
+        data['to_hour'] = '18:30'
+        data['date'] = datetime.strftime(datetime.now() - timedelta(days=1), '%Y-%m-%d')
+        response = self.client.post(url, data=data, format='json')
+        print('----------SPRAWDZANIE CZY DATA REZERWACJI JEST W PRZYSZŁOŚCI----------')
+        print(json.loads(response.content))
+        self.assertEqual(json.loads(response.content),
+                         {'error': 'Nie można zarezerwować sali w przeszłości'})
+        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
+        data['date'] = datetime.strftime(datetime.now() + timedelta(days=1), '%Y:%m:%d')
+        response = self.client.post(url, data=data, format='json')
+        print('----------SPRAWDZANIE CZY FORMAT DATY JEST PRAWIDŁOWY----------')
+        print(json.loads(response.content))
+        self.assertEqual(json.loads(response.content),
+                         {'error': 'Zły format daty. Prawidłowy format daty to RRRR-MM-DD'})
+        self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+        data['number_of_seats'] = 'dziesięć'
+        data['number_of_computers'] = 'dziesięć'
+        data['date'] = datetime.strftime(datetime.now() + timedelta(days=1), '%Y-%m-%d')
+        response = self.client.post(url, data=data, format='json')
+        print('----------SPRAWDZANIE CZY WYMAGANIA LICZBOWE SĄ PODANE POPRAWNIE----------')
+        print(json.loads(response.content))
+        self.assertEqual(json.loads(response.content),
+                         {'error': 'Liczba miejsc i komputerów musi być liczbą całkowitą!'})
+        self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
--- room/views.py
+++ room/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from django.http import JsonResponse
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -14,31 +14,40 @@ class RoomView(viewsets.ModelViewSet):
     serializer_class = RoomSerializer
 
 
-@api_view(['GET'])
+@api_view(['POST'])
 @permission_classes([AllowAny])
 def show_available_rooms(request):
-    if request.method == 'GET':
+    if request.method == 'POST':
         try:
-            if datetime.strptime(request.data['date'], '%Y-%m-%d').date() >= datetime.now().date():
-                base_hours = [datetime.strptime('8:00', '%H:%M').time(), datetime.strptime('9:45', '%H:%M').time(),
-                              datetime.strptime('11:30', '%H:%M').time(), datetime.strptime('13:15', '%H:%M').time(),
-                              datetime.strptime('15:00', '%H:%M').time(), datetime.strptime('16:45', '%H:%M').time(),
-                              datetime.strptime('18:30', '%H:%M').time()]
-                rooms = Room.objects.all()
+            data = datetime.strptime(request.data['date'], '%Y-%m-%d').date()
+        except ValueError:
+            data = {'error': 'Zły format daty. Prawidłowy format daty to RRRR-MM-DD'}
+            return Response(data=data, status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+        if data >= datetime.now().date():
+            base_hours = [datetime.strptime('8:00', '%H:%M').time(), datetime.strptime('9:45', '%H:%M').time(),
+                          datetime.strptime('11:30', '%H:%M').time(), datetime.strptime('13:15', '%H:%M').time(),
+                          datetime.strptime('15:00', '%H:%M').time(), datetime.strptime('16:45', '%H:%M').time(),
+                          datetime.strptime('18:30', '%H:%M').time()]
+            rooms = Room.objects.all()
+            if rooms:
                 fulfilled_requirements = []  # rooms that fulfil user's requirements
                 final_rooms = {}
                 for room in rooms:
-                    if (room.number_of_computers >= int(request.data['number_of_computers'])
-                            and room.number_of_seats >= int(request.data['number_of_seats'])
-                            and room.additional_equipment == request.data['additional_equipment']):
-                        fulfilled_requirements.append(room)
+                    try:
+                        if (room.number_of_computers >= int(request.data['number_of_computers'])
+                                and room.number_of_seats >= int(request.data['number_of_seats'])
+                                and room.additional_equipment == request.data['additional_equipment']):
+                            fulfilled_requirements.append(room)
+                    except ValueError:
+                        data = {'error': 'Liczba miejsc i komputerów musi być liczbą całkowitą!'}
+                        return Response(data=data, status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
                 reservations = Reservation.objects.all()
                 try:
                     hour_from = datetime.strptime(request.data['from_hour'], '%H:%M').time()
                     hour_to = datetime.strptime(request.data['to_hour'], '%H:%M').time()
                 except ValueError:
                     data = {'error': 'Zły format godziny. Prawidłowy format godziny to hh:mm'}
-                    return JsonResponse(data=data, status=500)
+                    return Response(data=data, status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
                 possible_hours = []
                 if hour_from < hour_to:
                     for hour in base_hours:
@@ -52,12 +61,13 @@ def show_available_rooms(request):
                             if reservation.hour in hours:
                                 hours.remove(reservation.hour)
                     final_rooms[str(fulfilled_room.number) + ' ' + str(fulfilled_room.wing)] = hours
-                return JsonResponse(final_rooms, status=200)
+                return Response(data=final_rooms, status=status.HTTP_200_OK)
             else:
-                data = {'error': 'Nie można zarezerwować sali w przeszłości'}
-                return JsonResponse(data=data, status=500)
-        except ValueError:
-            data = {'error': 'Zły format daty. Prawidłowy format daty to RRRR-MM-DD'}
-            return JsonResponse(data=data, status=500)
+                return Response(
+                    data={'errors': 'Nie znaleziono sal w bazie danych'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            data = {'error': 'Nie można zarezerwować sali w przeszłości'}
+            return Response(data=data, status=status.HTTP_406_NOT_ACCEPTABLE)
+
     else:
-        return Response(status=500)
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
