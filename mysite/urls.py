2134efec827a72143ece58385159f3b80fc85cd3
Paweł Młynarski <pawel.wybrzeze@gmail.com>
2019-12-21 12:49:52 +0100

    Added user login and register

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,13 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
+
+from user.views import user_register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rooms/', include('room.urls')),
     path('reservations/', include('reservation.urls')),
-    path('users/', include('user.urls')),
-    path('api-auth/', include(('rest_framework.urls')))
+    url(r'^login/', views.obtain_auth_token),
+    path('users/register', user_register, name='register'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,9 +1,24 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
 from .models import User
 from .serializers import UserSerializer
 
 
-class UserViews(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def user_register(request):
+    serializer = UserSerializer(data=request.data)
+    data = {}
+
+    if serializer.is_valid():
+        serializer.save()
+        data['success'] = 'Zarejestrowano pomyslnie'
+        return Response(data, status=status.HTTP_201_CREATED)
+    else:
+        print(serializer.data)
+        data['fail'] = "Błąd rejestracji"
+        return Response(data, status=status.HTTP_406_NOT_ACCEPTABLE)
 
